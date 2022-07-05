0902796112c41c718aba0fd6b8f589298be72f1e
Philipsdotdev <104207975+philipsdotdev@users.noreply.github.com>
2022-07-04 21:18:21 -0700

    A program that prints all possible different combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,30 +12,41 @@ int main(void)
 {
 	int i, j, k, l;
 
-	i = j = k = 48; l = 49;
+	i = j = k = 48; 
+	l = 49;
 	while  ((i < 58))
 	{
-		putchar(i); putchar(j); putchar(32); putchar(k); putchar(l);
+		putchar(i); 
+		putchar(j); 
+		putchar(32); 
+		putchar(k); 
+		putchar(l);
 		if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
 		{
 			putchar('\n'); i++;
 		}
 		else
 		{
-			putchar(44); putchar(32);
+			putchar(44); 
+			putchar(32);
 			if ((k == 57) && (l == 57))
 			{
 				if (j < 56)
 				{
-					l = ++j + 1; k = i;
+					l = ++j + 1; 
+					k = i;
 				}
 				else if (j == 56)
 				{
-					j++; k = i + 1; l = 48;
+					j++; 
+					k = i + 1; 
+					l = 48;
 				}
 				else if (j == 57)
 				{
-					j = 48; l = 49; k = ++i;
+					j = 48; 
+					l = 49; 
+					k = ++i;
 				}
 			}
 			else if (l < 57)
@@ -44,7 +55,8 @@ int main(void)
 			}
 			else
 			{
-				l = 48; k++;
+				l = 48; 
+				k++;
 			}
 		}
 	}
