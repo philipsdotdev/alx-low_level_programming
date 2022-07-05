2eb1fb4aad7f2e9ab6f649a3396843685841e3eb
Philipsdotdev <104207975+philipsdotdev@users.noreply.github.com>
2022-07-04 19:56:52 -0700

    A C program that prints a line of text followed by a new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
+/**
+ * main - Program starting point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
