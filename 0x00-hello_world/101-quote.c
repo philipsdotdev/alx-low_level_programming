de05e7d563432bbb0df62e941ffa0ee1c995b464
Philipsdotdev <104207975+philipsdotdev@users.noreply.github.com>
2022-07-04 20:19:21 -0700

    Print a line using format syntaxes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program starting point
+ *
+ * Return: Returns 0 after execution
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
