3d0396b12ff271c2d9b46965e534131cc53f727c
Philipsdotdev <104207975+philipsdotdev@users.noreply.github.com>
2022-07-05 16:33:46 -0700

    Print with _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,16 @@
 /**
  * main: program starting point
  *
- * description: prints with _putchar
+ * Description: prints with _putchar
  *
- * return: 0 after execution
+ * Return: 0 after execution
  */
 
 int main(void)
 {
 	int i = 0;
 	char c;
-	char s[] = "Alx School\n";
+	char s[] = "_putchar\n";
 
 	while (i <= 9)
 	{
