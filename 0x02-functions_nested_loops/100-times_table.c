d2cd67478248bf6d4415546e1162741fbb0b4984
Philipsdotdev <104207975+philipsdotdev@users.noreply.github.com>
2022-07-05 19:07:57 -0700

    Funtion to print the times table of any integer

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,56 +1,52 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - print the times table of any given integer
+ * main - print the sum of the even-values in a fibonacci
  *
- * @n: integer input
- *
- * Return: void
+ * Return: 0 after execution
  */
 
 void print_times_table(int n)
 {
-	int a, b, c, d, e, f;
+        int i, j, k;
 
-	if (n >= 0 && n < 15)
-	{
-		for (a = 0; a <=  n; a++)
-		{
-			for (b = 0; b <= n; b++)
-			{
-				c = a * b; d = c / 100; 
-				e = (c /  10) % 10;
-				f = (c % 100) % 10;
-				if (b == 0)
-				{
-					_putchar('0');
-				}
-				else if (c < 10)
-				{
-					_putchar(' '); 
-					_putchar(' ');
-					_putchar('0' + f);
-				}
-				else if (c < 100)
-				{
-					_putchar(' ');
-					_putchar('0' + e); 
-					_putchar('0' + f);
-				}
-				else
-				{
-					_putchar('0' + d);
-					_putchar('0' + e);
-					_putchar('0' + f);
-				}
-				if (b < n)
-				{
-					_putchar(','); 
-					_putchar(' ');
-				}
-				else
-					_putchar('\n');
-			}
-		}
-	}
+        if (n >= 0 && n <= 15)
+        {
+                for (i = 0; i <= n; i++)
+                {       
+                        for (j = 0; j <= n; j++)
+                        {
+                                k = j * i;
+                                if (j == 0)
+                                {
+                                        _putchar(k + '0');
+                                }
+                                else if (k < 10 && j != 0)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar(k + '0');
+                                }
+                                else if (k >= 10 && k < 100)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                        _putchar(' ');
+                                        _putchar((k / 10) + '0');
+                                        _putchar((k % 10) + '0');
+                                }
+                                else if (k >= 100)
+                                {
+                                        _putchar(',');
+                                        _putchar(' ');
+                                        _putchar((k / 100) + '0');
+                                        _putchar(((k / 10) % 10) + '0');
+                                        _putchar((k % 10) + '0');
+                                }
+                        }
+                        _putchar('\n');
+                }
+        }
 }
