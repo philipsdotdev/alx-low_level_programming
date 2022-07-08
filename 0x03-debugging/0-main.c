bd3705bba2ffe48d4f6825abf339df5623f00a9a
Philipsdotdev <104207975+philipsdotdev@users.noreply.github.com>
2022-07-07 20:40:54 -0700

    A function to test for positive or negative ()

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - A function to test for positive or negative ()
+  *
+  * Return: 0 after execution
+  */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
